62557dbdd96b61d7f7c5f7b1c656301f19c538cc
j65616030 <j65616030@gmail.com>
2024-11-22 14:58:20 -0300

    Create modelo.py

---
+++ modelo.py
@@ -0,0 +1,9 @@
+from database import db
+
+class Libro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        return {'id': self.id, 'titulo': self.titulo, 'autor': self.autor}
